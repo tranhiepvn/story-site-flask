dd1988417d7eb7bb922577bba1774a9d343c9e4c
Henry Tran <tranhiepvn@gmail.com>
2026-04-20 02:05:55 -0700

    fix_truyen_mot_nhieu_phan_xanh_duong_trên_trang_upload_luôn_fixed----Tét

--- src/app.py
+++ src/app.py
@@ -152,8 +152,9 @@ def get_category_groups() -> tuple[list["Category"], list["Category"], list["Cat
     """
     cats = Category.query.all()
     # define names for special groups. preserve order within these lists.
-    first_candidates_1 = ["Truyện Chỉ Có 1 Chương", "Phim Chỉ Có 1 Tập"]
-    first_candidates_2 = ["Truyện Có Nhiều Chương", "Phim Có Nhiều Tập"]
+    first_candidates_1 = ["Truyện một phần", "Truyện nhiều phần"]
+
+    first_candidates_2 = ["Phim có một tập", "Phim có nhiều tập"]
     # prepare containers
     group1: list[Category] = []
     group2: list[Category] = []
